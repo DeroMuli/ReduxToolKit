7208a5a97b8678ad6e7174308f93c1c10578351e
DeroMuli <musembimuli1999@gmail.com>
2022-10-20 10:09:19 +0300

    added message component

--- App.tsx
+++ App.tsx
@@ -1,24 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Provider } from 'react-redux';
+import { StyleSheet, Text, View , Button, SafeAreaView} from 'react-native';
+import { Provider , useDispatch, useSelector } from 'react-redux';
 import { store } from './store';
+import type { RootState } from './store';
+import { setMessage } from './message';
 
 export default function App() {
   return (
     <Provider store={store}>
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Message />
     </Provider>
   );
 }
+
+const Message = () => {
+  const dispatch = useDispatch();
+  const { message } = useSelector((state: RootState) => state.message);
+
+  const handlePress = () => {
+    dispatch(setMessage('Message from Component'));
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>{message}</Text>
+      <View style={styles.button}>
+      <Button title={'Set Message'} onPress={handlePress} />
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection : "row"
   },
+  text : {
+    margin : 10,
+    width : "60%",
+    marginTop : 30,
+    textAlign : "center",
+    textAlignVertical : "center",
+    height : 50
+  },
+  button : {
+    margin : 10,
+    height : 50,
+    marginTop : 40
+  }
 });
